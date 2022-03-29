55cdec0d39986319dc27f74376b9cab5cf668391
HANSOLKIM <hansolkim25@gmail.com>
2022-03-29 23:19:05 +0900

    render todo elements

--- case1_todo_list/assets/js/script.js
+++ case1_todo_list/assets/js/script.js
@@ -27,7 +27,7 @@
             <button class="todo_remove_button">
                 <i class="far fa-trash-alt"></i>
             </button>
-        </div>
+        </div>y
         <div class="item_buttons edit_buttons">
             <button class="todo_edit_confirm_button">
                 <i class="fas fa-check"></i>
@@ -39,10 +39,36 @@
         `
         return $todoItem;
     }
-    // js 실행 시 바로 실행되도록 함.
-    const init = () => {
-        const url = '';
-        fetch(url).then((response) => this = response.json())
+
+    // .todos DOM element 가져오기
+    const $todos = get('.todos');
+
+    // todos 렌더링
+    const renderAllTodos = (todos) => {
+        // .todos DOM element 초기화
+        $todos.innerHTML = '';
+        todos.forEach((item) => {
+            const todoElement = createTodoElement(item);
+            $todos.appendChild(todoElement);
+        });
     }
+
+    const getTodos = () => {
+        // axios의 경우 json() 형변환 없이 사용 가능하나, fetch는 필요.
+        fetch('http://localhost:3000/todos')
+        .then((response) => response.json())
+        .then((todos) => renderAllTodos(todos))
+        .catch((error) => console.error(error))
+    }
+
+    // js 실행 시 바로 실행되도록 하는 init 익명함수
+    const init = () => {
+        // DOMContentLoaded(초기 html 문서를 완전히 불러오고 분석했을 때) 시, fetch 발생.
+        window.addEventListener('DOMContentLoaded', (event) => {
+            getTodos();
+        });
+    }
+
+
     init();
 })();
