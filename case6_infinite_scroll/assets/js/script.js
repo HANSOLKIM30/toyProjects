663154d977a926534610712e1c9d68793883f469
hskimInfocar <51072405+HANSOLKIM30@users.noreply.github.com>
2022-04-05 17:30:37 +0900

    js 파일 기초작업
    - 구현 기능에 대한 설명 주석 기재
    - get: util 함수 선언

---
+++ case6_infinite_scroll/assets/js/script.js
@@ -0,0 +1,14 @@
+// 무한스크롤: 프로그래스바가 끝까지 닿았을 때 API로 또 다른 데이터 호출
+// pagination 기능을 통한 무한스크롤 구현: 1페이지가 끝나면 2페이지의 데이터를 호출.
+// API 호출 시, then-catch 대신 async-await 사용
+
+(function() {
+    
+    'use strict';
+
+    const get = (target) => {
+        return document.querySelector(target);
+    }
+
+
+})();
