8217dc0f6dbdb219fb5d320d69ca2070a9b6cc96
hskimInfocar <51072405+HANSOLKIM30@users.noreply.github.com>
2022-04-05 15:18:10 +0900

    baseball 기능 구현

--- case3_baseball/assets/js/script.js
+++ case3_baseball/assets/js/script.js
@@ -35,7 +35,7 @@
         //  생성할 패스워드
         let password = '';
 
-        while(password.length < limit) {
+        while(password.length < digit) {
             // parseInt의 두번째 인자인 radix를 통해 10진수로 만들어주기
             const random = parseInt(Math.random() * 10, 10);
             if(gameLimit[random]===true) {
@@ -45,10 +45,9 @@
 
             password += random;
             gameLimit[random] = true;
-
         }
 
-        
+        baseball.password = password;
     }
 
     const onPlayed = (number, hit) => {
@@ -58,22 +57,64 @@
 
     const isCorrect = (inputNumber, password) => {
         // 번호가 같은가?
+        return inputNumber === password;
     }
 
-    const isDuplicate = () => {
+    const isDuplicate = (inputNumber) => {
         // 중복번호가 있는가?
+        /*
+        * Set
+        * 새로운 배열을 중복없이 반환하는 Set
+        * 하나의 Set 내의 값은 한 번만 나타날 수 있다. 즉, 어떤 값은 그 Set 콜렉션 내에서 유일하다.
+        */
+        return [...new Set(inputNumber.split(''))].length !== digit;
     }
 
-    const getStrikes = () => {
+    const getStrikes = (inputNumber, password) => {
         // 스트라이크 카운트는 몇 개?
+        let strike = 0;
+        const nums = inputNumber.split('');
+
+        nums.map((num, index) => {
+            if(num === password[index]) {
+                strike++;
+            }
+        })
+        return strike;
     }
 
-    const getBalls = () => {
+    const getBalls = (inputNumber, password) => {
         // 볼 카운트는 몇 개?
+        // ball =>  숫자가 해당 숫자의 위치가 아니지만, password에 있는 숫자일 때 
+        let ball = 0;
+        const nums = inputNumber.split('');
+        const gameLimit = Array(limit).fill(false);
+        
+        password.split('').map((digit) => {
+            gameLimit[digit] = true;
+        });
+
+        nums.map((num, index) => {
+            
+            if(password[index] !== num && gameLimit[num]) {
+                ball++;
+            }
+        });
+
+        return ball;
     }
 
     const getResult = (inputNumber, password) => {
         // 시도에 따른 결과는?
+        if(isCorrect(inputNumber, password)) {
+            end = true;
+            $answer.innerHTML = baseball.password;
+            return '홈런!!';
+        } 
+        const strikes = getStrikes(inputNumber, password); 
+        const balls = getBalls(inputNumber, password);
+        
+        return `STRIKE: ${strikes}, BALL: ${balls}`;
     }
 
     const playGame = (event) => {
@@ -93,7 +134,7 @@
         
         } else if(isDuplicate(inputNumber)) {
         
-            alert('중복 숫자가 있습니다.')
+            alert('중복 숫자가 있습니다.');
         
         } else {
 
@@ -101,17 +142,16 @@
             trial++;
         
             const result = onPlayed(inputNumber, getResult(inputNumber, password));
-            $question.innerHtml += `<span>${ result }</span>`
-
+            $question.innerHTML += `<span>${ result }</span>`;
             // 시도횟수를 넘어서고, 답이 틀렸을 경우
             if(limit <= trial && !isCorrect(inputNumber, password)) {
                 alert('쓰리아웃!');
                 end = true;
-                $answer.innerHtml = password;
+                $answer.innerHTML = password;
             }
         }
 
-        // 각각으 시도가 끝날 때마다 $input 값을 비워주고 포커싱 처리
+        // 각각의 시도가 끝날 때마다 $input 값을 비워주고 포커싱 처리
         $input.value = '';
         $input.focus();
     }
