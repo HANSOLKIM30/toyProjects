5ac22159ff7337322769bb9836aede4eadf710e6
hskimInfocar <51072405+HANSOLKIM30@users.noreply.github.com>
2022-04-06 14:27:00 +0900

    Update script.js

--- case6_infinite_scroll/assets/js/script.js
+++ case6_infinite_scroll/assets/js/script.js
@@ -25,7 +25,7 @@
         const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`;
         const response = await fetch(API_URL);
         if(!response.ok) {
-            throw new Error('에러')
+            throw new Error('에러');
         }
 
         return await response.json();
