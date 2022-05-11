3f17c6767f17e9a7c09baae4ac4de3e2c5ddd97f
HANSOLKIM <hansolkim25@gmail.com>
2022-05-11 16:50:02 +0900

    photo editor 마크업

---
+++ case19_editor_photo/assets/js/script.js
@@ -0,0 +1,12 @@
+/*
+[Used API]
+1. FileReader
+https://developer.mozilla.org/ko/docs/Web/API/FileReader
+
+2. crop
+https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+
+3. drag and drop
+https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+*/
+
